626162394b05097c5da51f788bc09228f06878a4
TERRINO9 <terrino9@posteitaliane.it>
2023-06-23 15:12:47 +0200

    a

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config();
+require('dotenv').config(); 
 
 // Imposta il token del tuo bot
 const token = process.env.TELEGRAM_TOKEN;
