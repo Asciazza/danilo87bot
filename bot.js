40a93b3d2ad22bbb5f1b05fd2ed8f0d39c8aae52
TERRINO9 <terrino9@posteitaliane.it>
2023-06-23 10:24:21 +0200

    a

--- bot.js
+++ bot.js
@@ -3,9 +3,31 @@ require('dotenv').config();
 
 // Imposta il token del tuo bot
 const token = process.env.TELEGRAM_TOKEN;
+const serverPort = process.env.SERVER_PORT;
 
 // Crea una nuova istanza del bot
 const bot = new TelegramBot(token, { polling: true });
+
+const http = require('http');
+
+http.createServer((request, response) => {
+  request.on('error', (err) => {
+    console.error(err);
+    response.statusCode = 400;
+    response.end();
+  });
+  response.on('error', (err) => {
+    console.error(err);
+  });
+  if (request.method === 'GET' && request.url === '/ping') {
+    response.statusCode = 200;
+    response.write("pong");
+    response.end();
+  } else {
+    response.statusCode = 404;
+    response.end();
+  }
+}).listen(serverPort);
 
 // Gestisce il comando /start
 bot.onText(/\/start/, (msg) => {
